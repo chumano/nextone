fe7d65db4827e7dcbe0e9b10fd79b137b7f12391
chumano <xuanloc_hoang2000@yahoo.com>
2021-10-16 14:47:13 +0700

    add identityservice

---
+++ src/IdentityService/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityService
+{
+    public static class Messages
+    {
+        public const string TITLE = "Next One - SSO";
+        public const string APP_ANME = "Next One";
+        public const string LOGIN = "Đăng nhập";
+        public const string REMEMBER_MY_LOGIN = "Nhớ mật khẩu";
+    }
+}
