3bb66ff5f02a8e2c083e9c957b564891e67543ed
chumano <xuanloc_hoang2000@yahoo.com>
2022-05-20 09:11:29 +0700

    update migration eventtype

---
+++ src/ComService/Migrations/20220520020923_Com_UpdateEventType.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComService.Migrations
+{
+    public partial class Com_UpdateEventType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IconUrl",
+                schema: "com",
+                table: "T_App_EventType",
+                type: "nvarchar(255)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                schema: "com",
+                table: "T_App_EventType",
+                type: "nvarchar(255)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IconUrl",
+                schema: "com",
+                table: "T_App_EventType");
+
+            migrationBuilder.DropColumn(
+                name: "Note",
+                schema: "com",
+                table: "T_App_EventType");
+        }
+    }
+}
