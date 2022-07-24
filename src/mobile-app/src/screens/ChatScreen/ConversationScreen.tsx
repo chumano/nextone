bb6e507c68aae0b9e6a41c0098f774b93fdd7c9d
chumano <xuanloc_hoang2000@yahoo.com>
2022-07-24 08:57:17 +0700

    move getconversationlist to conversation screen

--- src/mobile-app/src/screens/ChatScreen/ConversationScreen.tsx
+++ src/mobile-app/src/screens/ChatScreen/ConversationScreen.tsx
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import React, { useCallback, useEffect } from 'react';
 import {View, StyleSheet} from 'react-native';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { conversationApi } from '../../apis';
 
 import ConversationList from '../../components/Conversation/ConversationList';
-import { IAppStore } from '../../stores/app.store';
+import { AppDispatch, IAppStore } from '../../stores/app.store';
 import { conversationActions } from '../../stores/conversation';
+import {getListConversation} from '../../stores/conversation/conversation.thunk';
 
 const ConversationScreen = () => {
-  const dispatch = useDispatch();
+  const dispatch: AppDispatch = useDispatch();
   const {notLoadedConversationId} = useSelector(
     (store: IAppStore) => store.conversation,
   );
+
   useEffect(() => {
     if (!notLoadedConversationId) return;
     const fetchConversation = async () => {
@@ -24,6 +27,19 @@ const ConversationScreen = () => {
     }
     fetchConversation();
   }, [dispatch, notLoadedConversationId])
+  
+  useEffect(() => {
+    dispatch(getListConversation({pageOptions: {offset: 0}}));
+  }, [dispatch]);
+  
+  useFocusEffect(useCallback(() => {
+    console.log("ConversationScreen is focused") 
+    //dispatch(getListConversation({pageOptions: {offset: 0}}));
+    return () => {
+      console.log("ConversationScreen is outfocused") 
+    };
+  }, []));
+
   
   return (
     <View style={styles.conversationScreenContainer}>
